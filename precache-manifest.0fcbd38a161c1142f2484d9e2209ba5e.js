687b08796300c51762943bfb0c74bb34b10d37b0
azmanzaki <m.azman.zaki@gmail.com>
2019-11-20 14:43:57 +0800

    Updates

---
+++ precache-manifest.0fcbd38a161c1142f2484d9e2209ba5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20bf6187ac9586acb4701026a63220d",
+    "url": "/userapp/index.html"
+  },
+  {
+    "revision": "6e203ee66dc050783159",
+    "url": "/userapp/static/css/main.ce461145.chunk.css"
+  },
+  {
+    "revision": "b6d51a5d7e50311ef88f",
+    "url": "/userapp/static/js/2.78b7d3c9.chunk.js"
+  },
+  {
+    "revision": "6e203ee66dc050783159",
+    "url": "/userapp/static/js/main.5279ee98.chunk.js"
+  },
+  {
+    "revision": "38947430c536da80c036",
+    "url": "/userapp/static/js/runtime-main.d10d0449.js"
+  }
+]);
